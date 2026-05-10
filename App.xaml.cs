87a009406357b7f1cd95af58ecf0556362243d03
yusufemrekaraca <yusufemrekrc06@gmail.com>
2026-05-11 01:09:14 +0300

    Apply ModernWpf dark theme

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,12 @@
+using ModernWpf;
+
 namespace UsbSecureVault;
 
 public partial class App : System.Windows.Application
 {
+    protected override void OnStartup(System.Windows.StartupEventArgs e)
+    {
+        ThemeManager.Current.ApplicationTheme = ApplicationTheme.Dark;
+        base.OnStartup(e);
+    }
 }
